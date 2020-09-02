3d972e90cf90886fe89bcd5f080a82331d9c06b3
MaliszewskiKamil <kamcio4112@gmail.com>
2020-09-02 15:57:48 +0200

    first chapter, some excercises + classes from book

---
+++ sedgewick/chapter1/chapterOne/src/chapterone/ChapterOne.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapterone;
+
+import algs4.StdDraw;
+
+/**
+ *
+ * @author crypt0
+ */
+public class ChapterOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String x = "hahaha";
+        String y = "XDXDXD";
+        
+        System.out.println(x+y);
+        
+    }
+    
+}
