8f792e6708b743689c29040e4826f641402d07a9
MaliszewskiKamil <kamcio4112@gmail.com>
2020-09-02 17:59:29 +0200

    Started implementing the UML

---
+++ sedgewick/Assignment1/Trains-Startproject/src/main/java/models/FreightWagon.java
@@ -0,0 +1,17 @@
+package models;
+// TODO
+
+public class FreightWagon extends Wagon {
+
+	private final int maxWeight;
+
+	public FreightWagon(int wagonId, int maxWeight) {
+		super(wagonId);
+		this.maxWeight = maxWeight;
+	}
+
+	public int getMaxWeight() {
+		return maxWeight;
+	}
+
+}
